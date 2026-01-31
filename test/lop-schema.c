3e123912672ab670b9b9027f7d3f892720e5faad
Aleksey Kuleshov <rndfax@yandex-team.ru>
2026-02-01 01:42:58 +0300

    WIP

--- test/lop-schema.c
+++ test/lop-schema.c
@@ -1,14 +1,30 @@
 #include <LOP.h>
+#include <string.h>
 #include "FileMap.h"
 
 static int cb_dummy(struct LOP_HandlerList hl, struct LOP_ASTNode *n, void *param, void *cb_arg)
 {
+	static int level;
+
+	for (int i = 0; i < level; i++) {
+		printf("\t");
+	}
+
+	if (n->type > LOP_TYPE_LIST_LAST) {
+		printf("%s %s\n", (char *)cb_arg, LOP_symbol_value(n));
+	} else {
+		printf("%s\n", (char *)cb_arg);
+	}
+	level++;
+	LOP_cb_default(hl, n, param, NULL);
+	level--;
 	return 0;
 }
 
 static int resolve(struct LOP *lop, const char *key, struct LOP_CB *cb)
 {
 	cb->func = cb_dummy;
+	cb->arg = strdup(key);
 	return 0;
 }
 
